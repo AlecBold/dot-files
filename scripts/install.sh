2e9b69390f854c1aaab7f186a28a0e21f63596ef
sandbox <alecbold@yandex.com>
2021-02-23 13:55:47 +0300

    [install] fix syntax

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,12 +2,22 @@
 
 set -e
 
-DOT_ROOT=~/dot-files
-REPO=AlecBold/dot-files
-BRANCH=master
-REMOTE=https://github.com/${REPO}
+DOT_ROOT="~/dot-files"
+REPO="AlecBold/dot-files"
+BRANCH="master"
+REMOTE="https://github.com/${REPO}"
 
-apt_progs=(firefox htop adb git zsh chromium-browser vim python tree)
+apt_progs=(
+	firefox
+       	htop
+       	adb
+       	git
+       	zsh
+       	chromium-browser
+       	vim
+       	python
+       	tree
+)
 
 
 
